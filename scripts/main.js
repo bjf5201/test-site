cea3117b137eb671811b74be6e22c59f73889555
Bethany <bjf5201@uncw.edu>
2021-01-25 17:37:06 -0500

    Add interactive welcome message and a <<Change User>> button that works with the local storage API

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,34 @@ myImage.onclick = function() {
         else {
             myImage.setAttribute('src', 'images/hero-img_large2.png');
     }
+};
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Welcome, ' + myName;
+};
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome, ' + storedName;
+};
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+
+myButton.onclick = function() {
+    setUserName();
 }
